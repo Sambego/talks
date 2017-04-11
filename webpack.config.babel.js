cea31610935113fc36f5642c2d6d1c62936b082d
Sambego <sambellen@gmail.com>
2017-04-11 09:14:21 +0200

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,54 @@
+import webpack from 'webpack';
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const createConfig = (env = 'development') => {
+    return {
+        entry: {
+            script: './src/index.js',
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].[hash].js',
+        },
+        module: {
+            rules: [{
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: ['style-loader'],
+                    use: ['css-loader?importLoaders=1', 'postcss-loader', 'sass-loader'],
+                }),
+            }, {
+                test: /\.(woff|woff2|svg|jpg)$/,
+                use: 'file-loader?name=[name].[ext]',
+            }],
+        },
+        devtool: 'source-map',
+        plugins: [
+            new webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: false,
+            }),
+            new ExtractTextPlugin({
+                filename: '[name].[contenthash].css',
+                allChunks: true,
+                disable: env !== 'production',
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+            }),
+            new CopyWebpackPlugin([{
+                context: './src/icons/',
+                from: '**/*',
+            }, {
+                context: './src/images/',
+                from: '**/*',
+            }]),
+        ],
+    };
+};
+
+module.exports = createConfig;
